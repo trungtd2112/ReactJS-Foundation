78bcf954158f7f50eceb416d8c873329017cac35
Đức Trung <ductrung.2112xx@gmail.com>
2021-02-02 10:25:18 +0700

    function definition

--- 01/vd1/src/App.js
+++ 01/vd1/src/App.js
@@ -1,14 +1,54 @@
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+function One(){
+  return (
+    <div>
+      <h1>Cach so 1</h1> 
+      <h2>Cach so 2</h2>
+    </div>
+  )
+}
+
+var Two = function(){
+  return(
+
+    <div>
+      <h3>
+        ahihi
+      </h3>
+    </div>
+  );
+}
+
+var Three = () => (
+  <div>
+    anh yeu em
+  </div>
+);
+
+class Four extends Component {
+  render() {
+    return (
+      <div>
+        <button type="button" class="btn btn-outline-primary">anh yeu em</button>
+      </div>
+    );
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Trung dz vcl <br></br> Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <One></One>
+        <Two></Two>
+        <Three></Three>
+        <Four></Four>
         <a
           className="App-link"
           href="https://reactjs.org"
