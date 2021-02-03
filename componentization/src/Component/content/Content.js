7a87de308e442cab979d7308f0dc9b6ccf210b6f
Đức Trung <ductrung.2112xx@gmail.com>
2021-02-03 10:11:39 +0700

    interacts through state

--- componentization/src/Component/content/Content.js
+++ componentization/src/Component/content/Content.js
@@ -3,9 +3,54 @@ import React, { Component } from 'react';
 class Content extends Component {
     constructor(props) {
         super(props);
-        
+        this.state = {
+            trangThai: 0
+        }
+    }
+
+    thongbao = () => { alert("Hai Tu xinh dep"); }
+
+    renderButton = () => {
+        return (
+            <div className="btn btn-group">
+                <div className="btn btn-warning" onClick = {() => this.editClick()}>Edit</div>
+                <br/>
+                <div className="btn btn-danger" >Delete</div>
+            </div>
+        );
+    }
+
+    renderForm = () => {
+        return (
+            <div className="form-group">
+                <input defaultValue={this.props.tieude} type="text" name="ten" className="form-control" />
+                <div className="btn btn-info float-left mt-10" onClick = {() => this.saveClick()}>Save</div>
+            </div>
+        );
+    };
+
+    displayCheck = () => {
+        if (this.state.trangThai === 0) {
+            return this.renderButton();
+        }else{
+            return this.renderForm();
+        }
+    }
+
+    editClick = () => {
+        this.setState({
+            trangThai : 1
+        });    
+    }
+
+    saveClick = () => {
+        this.setState({
+            trangThai : 0
+        });    
     }
     
+
+
     render() {
         return (
             <section>
@@ -21,6 +66,9 @@ class Content extends Component {
                                 <h2 className="display-4">{this.props.tieude}</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
                             </div>
+
+                            {this.displayCheck()}
+
                         </div>
                     </div>
                 </div>
