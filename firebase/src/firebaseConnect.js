a48d0d3d3191319eee3981b76d2d3e0f12916974
Đức Trung <ductrung.2112xx@gmail.com>
2021-02-17 10:52:14 +0700

    end firebase

--- firebase/src/firebaseConnect.js
+++ firebase/src/firebaseConnect.js
@@ -4,14 +4,14 @@ import 'firebase/database';
 
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBQWZJvA21FoV63dyGg58d2_zWmIzgV1N4",
-  authDomain: "note-f977e.firebaseapp.com",
-  databaseURL: "https://note-f977e-default-rtdb.firebaseio.com",
-  projectId: "note-f977e",
-  storageBucket: "note-f977e.appspot.com",
-  messagingSenderId: "675968495630",
-  appId: "1:675968495630:web:efc9e3eb8afa6f554b0c5f",
-  measurementId: "G-VM9HC6GPXT"
+  apiKey: "AIzaSyCP_09-wMS67U5cznJ3f6LcM4VK6EztXRE",
+  authDomain: "lazy-cdd98.firebaseapp.com",
+  databaseURL: "https://lazy-cdd98-default-rtdb.firebaseio.com",
+  projectId: "lazy-cdd98",
+  storageBucket: "lazy-cdd98.appspot.com",
+  messagingSenderId: "849967208443",
+  appId: "1:849967208443:web:47544e75a31d7b1e8f5d5e",
+  measurementId: "G-D415VDV7ZB"
 };
 
 export const fbConnect = firebase.initializeApp(firebaseConfig);
